e7c62ba776acc9bc37094483eb338c0ec7729cff
AnonymousWall <kxf450fmx@gmail.com>
2026-01-30 23:06:06 -0800

    First Commit

---
+++ AnonymousWallIosTests/AnonymousWallIosTests.swift
@@ -0,0 +1,17 @@
+//
+//  AnonymousWallIosTests.swift
+//  AnonymousWallIosTests
+//
+//  Created by Ziyi Huang on 1/30/26.
+//
+
+import Testing
+@testable import AnonymousWallIos
+
+struct AnonymousWallIosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
